e2c5358c92e37a602fe1316bb5385b8740fc6338
Glocean <211661684+gloceansh@users.noreply.github.com>
2025-07-30 18:59:12 +0400

    Update weather plugin to be dynamic

--- sketchybar/plugins/weather.sh
+++ sketchybar/plugins/weather.sh
@@ -3,28 +3,35 @@
 sleep 10
 
 API_KEY="YOUR_API_KEY"
-LAT="YOUR_LATITUDE"
-LON="YOUR_LONGITUDE"
+LOCATION_DATA=$(curl -sf "http://ip-api.com/json")
+LAT=$(echo "$LOCATION_DATA" | jq -r '.lat')
+LON=$(echo "$LOCATION_DATA" | jq -r '.lon')
 URL="https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${API_KEY}&units=metric"
 
 DATA=$(curl -sf "$URL")
 if [ -z "$DATA" ]; then
-  sketchybar --set weather label="􁜏 "
+  sketchybar --set weather label="􁜏  "
   exit 1
 fi
 
 TEMP=$(echo "$DATA" | jq '.main.temp' | cut -d "." -f 1)
 CONDITION=$(echo "$DATA" | jq -r '.weather[0].main')
+CONDITION_DESC=$(echo "$DATA" | jq -r '.weather[0].description')
 
-case "$CONDITION" in
-  Thunderstorm) ICON="􀇓 " ;;
-  Drizzle)      ICON="􀇗 " ;;
-  Rain)         ICON="􀇇 " ;;
-  Snow)         ICON="􀇏 " ;;
-  Clear)        ICON="􀇕 " ;;
-  Clouds)       ICON="􀇃 " ;;
-  Mist|Fog|Haze|Smoke|Dust|Ash|Sand) ICON="􀇣 " ;;
-  *)            ICON="􀁝 " ;;
+case "$CONDITION_DESC" in
+  "few clouds") ICON="􀇕 " ;;
+  *)
+    case "$CONDITION" in
+      Thunderstorm) ICON="􀇟 " ;;
+      Drizzle)      ICON="􀇅 " ;;
+      Rain)         ICON="􀇇 " ;;
+      Snow)         ICON="􀇏 " ;;
+      Clear)        ICON="􀆮 " ;;
+      Clouds)       ICON="􀇃 " ;;
+      Mist|Fog|Haze|Smoke|Dust|Ash|Sand) ICON="􀇣 " ;;
+      *)            ICON="􀁝 " ;;
+    esac
+    ;;
 esac
 
 sketchybar --set weather label="$ICON  ${TEMP}°C"
