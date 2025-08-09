eeacc08502b653806238552e8290af46dd66ee9e
Glocean <211661684+gloceansh@users.noreply.github.com>
2025-08-09 14:02:06 +0400

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,5 @@
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
-fish_add_path /Users/glocean/.cargo/bin
-fish_add_path /Users/glocean/.spicetify
 
 fish_config theme choose "Rosé Pine"
 
