eae894e6f0f46efe14d6317f42a1be93f692a3ea
Glocean <211661684+gloceansh@users.noreply.github.com>
2025-10-01 07:49:06 +0400

    Change fish theme to 'Everforest' and add instructions
    Updated the theme configuration in fish to use 'Everforest' and added installation instructions.

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,8 @@
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
 
-fish_config theme choose "Rosé Pine"
+# make sure to install the theme first, which can be done done with fisher: fisher install Arpitpandey992/fish-everforest
+fish_config theme choose "Everforest"
 
 alias neofetch='fastfetch -c neofetch'
 
@@ -10,4 +11,3 @@ neofetch
 set -U fish_greeting
 
 starship init fish | source
-
